3481484305018b804232e73f95bd7b48d26a77e6
Eli <78963198+niceEli@users.noreply.github.com>
2024-04-29 22:19:46 -0400

    idk

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 @onready var timer = $Timer
+@onready var death_sound = $DeathSound
 
 func _on_body_entered(body):
 	print("You Died!")
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	death_sound.play()
 	timer.start()
 
 
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var jump = $Jump
+@onready var land = $Land
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
@@ -8,6 +10,7 @@ const JUMP_VELOCITY = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var floored : int = 0
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,7 +20,15 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		jump.play()
+		
+	if is_on_floor() && floored == 0:
+		floored += 1
+		land.play()
+	else:
+		if !is_on_floor():
+			floored = 0
+		
 	# Get the input direction: -1, 0, 1
 	var direction = Input.get_axis("MoveLeft", "MoveRight")
 	
