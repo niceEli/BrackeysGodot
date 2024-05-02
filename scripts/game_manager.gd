5a9be3244648bbd43f99055ba6cd71d815e3e3ad
Eli <78963198+niceEli@users.noreply.github.com>
2024-05-02 16:37:37 -0400

    true w

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -13,6 +13,33 @@ func add_point():
 func quit_game():
 	get_tree().quit()
 
-func _input(event):
+func _ready():
+	var config = ConfigFile.new()
+	var err = config.load("user://settings.cfg")
+	
+	# If the file didn't load, ignore it.
+	if err != OK:
+		config.save("user://settings.cfg")
+	DisplayServer.window_set_mode(config.get_value("screen", "mode", 0))
+
+func _input(_event):
 	if Input.is_action_just_pressed("Pause"):
 		quit_game()
+	if Input.is_action_just_pressed("Fullscreen"):
+		var config = ConfigFile.new()
+		var err = config.load("user://settings.cfg")
+		
+		# If the file didn't load, ignore it.
+		if err != OK:
+			config.save("user://settings.cfg")
+		var wind := DisplayServer.window_get_mode()
+		if wind == 0:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+			config.set_value("screen", "mode", 3)
+		elif wind == 3:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			config.set_value("screen", "mode", 0)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+			config.set_value("screen", "mode", 0)
+		config.save("user://settings.cfg")
